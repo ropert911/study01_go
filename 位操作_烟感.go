280b17ed5ee3e05a110db019a1f9de845b1fc8c6
ropert911 <254323626@qq.com>
2019-06-12 17:05:29 +0800

    添加示例

--- 位操作_烟感.go
+++ 位操作_烟感.go
@@ -5,22 +5,21 @@ import (
 	"strconv"
 )
 
+//博频-烟感 版本号:v1.03
 type SmokeSensor struct {
-	b1aggVersion uint8 //0x0f	协议版本 当前2
-	b1sofVersion uint8 //0xf0  软件版本 当前2
-	//0x0f  消息类型   1.报警  2.报警静音(静音时间小于100 秒)  3.故障   4.低压  7.正常待机
-	// 测试：当松开测试按键时RF 上传报警器状态，例如当前烟感报警器为电池低压时，按测试键将上传0x14； 其他保留  心跳每天一次（心跳上传消息类型为报警器当前的状态）
-	b2mesType uint8
-	//0xf0  设备类型  1.烟感  2.热感-保留   3.一氧化碳-保留 4.可燃气体 5.温度+烟雾复合型报警器，保留（未使用）
-	//6.一氧化碳+烟雾复合型报警器，保留（未使用）
-	//7.一氧化碳+烟雾+温度复合型报警器，保留（未使用）；
-	//8.一氧化碳+可燃气体复合型报警器，保留（未使用）；
-	//9.PIR 探测器，保留（未使用）；
-	//A.漏水报警器，保留（未使用）；
-	//B.门磁探测器，保留（未使用）；
-	//C.温湿探测器，保留（未使用）；
-	//其他保留
-	b2devType uint8
+	b1devType    uint8 //0xf0	1表示为烟雾报警器
+	b1sofVersion uint8 //0x0f   协议版本 当前3
+	//设备状态：
+	//BIT7:保留，默认为0；
+	//BIT6:保留，默认为0；
+	//BIT5:等于1表示本地测试报警(当传感器故障时按测试键不置位此标记位)；
+	//BIT4:等于1表示报警记忆；
+	//BIT3:等于1表示烟雾报警器电池电压过低；
+	//BIT2:等于1表示烟雾报警器处于传感器故障；
+	//BIT1:等于1表示烟雾报警器处于报警静音；
+	//BIT0:等于1表示烟雾报警器侦测到烟雾进入报警；
+	b2devStatus uint8
+
 	//当前报警器电池电压百分比
 	b3volPercentage uint8
 	//当前报警器烟雾浓度百分比
@@ -32,39 +31,37 @@ type SmokeSensor struct {
 }
 
 func smokeTest() {
-	var smokeSensor SmokeSensor
-	smokeSensor.b1aggVersion = 2
-	smokeSensor.b1sofVersion = 2
-	smokeSensor.b2mesType = 7
-	smokeSensor.b2devType = 1
-	smokeSensor.b3volPercentage = 90
-	smokeSensor.b4smokePercentage = 2
-	smokeSensor.b5Status = 0
+	var scData SmokeSensor
+	scData.b1devType = 1
+	scData.b1sofVersion = 3
+	scData.b2devStatus = 0x20 //测试消息
+	scData.b3volPercentage = 90
+	scData.b4smokePercentage = 2
+	scData.b5Status = 0
 	var b1 uint8
-	b1 = smokeSensor.b1aggVersion | (smokeSensor.b1sofVersion << 4)
+	b1 = scData.b1sofVersion | (scData.b1devType << 4)
 	var b2 uint8
-	b2 = smokeSensor.b2mesType | (smokeSensor.b2devType << 4)
+	b2 = scData.b2devStatus
 	var sum uint8
-	sum = b1 + b2 + smokeSensor.b3volPercentage + smokeSensor.b4smokePercentage + smokeSensor.b5Status
-	smokeSensor.b6checkSum = ^sum + 1
+	sum = b1 + b2 + scData.b3volPercentage + scData.b4smokePercentage + scData.b5Status
+	scData.b6checkSum = ^sum + 1
 
-	fmt.Println(smokeSensor)
+	fmt.Println(scData)
 
-	data := fmt.Sprintf("%02x%02x%02x%02x%02x%02x", b1, b2, smokeSensor.b3volPercentage, smokeSensor.b4smokePercentage, smokeSensor.b5Status, smokeSensor.b6checkSum)
+	var data = fmt.Sprintf("%02x%02x%02x%02x%02x%02x", b1, b2, scData.b3volPercentage, scData.b4smokePercentage, scData.b5Status, scData.b6checkSum)
 	fmt.Println(data)
 
 	{
 		var smokeSensor SmokeSensor
 		var b uint8
-		data = "132064000069"
+		//data = "132064000069"
 		v1, _ := strconv.ParseUint(data[0:2], 16, 16)
 		b = uint8(v1)
-		smokeSensor.b1aggVersion = b & 0x0f
-		smokeSensor.b1sofVersion = (b & 0xf0) >> 4
+		smokeSensor.b1sofVersion = b & 0x0f
+		smokeSensor.b1devType = (b & 0xf0) >> 4
 		v1, _ = strconv.ParseUint(data[2:4], 16, 16)
 		b = uint8(v1)
-		smokeSensor.b2mesType = b & 0x0f
-		smokeSensor.b2devType = (b & 0xf0) >> 4
+		smokeSensor.b2devStatus = b
 		v1, _ = strconv.ParseUint(data[4:6], 16, 16)
 		smokeSensor.b3volPercentage = uint8(v1)
 		v1, _ = strconv.ParseUint(data[6:8], 16, 16)

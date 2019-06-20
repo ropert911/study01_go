def9ebed7763423a306f2858e1ca1aa46b83f2ed
root <254323626@qq.com>
2019-06-20 23:07:20 +0800

    xxx

--- 时间.go
+++ 时间.go
@@ -6,7 +6,24 @@ import (
 )
 
 func main() {
-	var t = time.Now()
+	//打印utc的纳秒、微秒、毫秒
+	//testTime1()
+
+	//时间格式化显示
+	//testTime2()
+
+	//设置时间
+	//testTime3()
+
+	//从字符串解析出时间
+	//testTime4()
+
+	//时间加减运算
+	testTime5()
+}
+
+func testTime1() {
+	var t = time.Now() //返回Time构建
 	nans := t.UnixNano()
 	micros := t.UnixNano() / int64(time.Microsecond)
 	mills := t.UnixNano() / int64(time.Millisecond)
@@ -16,52 +33,61 @@ func main() {
 	fmt.Println("微秒：", micros)
 	fmt.Println("毫秒：", mills)
 	fmt.Println("秒：", secs)
-
-	//2019-06-06T15:58:11+08:00
-	{
-		fmt.Println("")
-		nowTime := time.Now()
-		t1 := nowTime.String()
-		timeStr := t1[:19]
-		fmt.Println(timeStr)
-	}
-
-	{
-		fmt.Println("")
-		const shortForm = "2006-01-01 15:04:05"
-		t := time.Now()
-		temp := time.Date(t.Year(), t.Month(), t.Day(), t.Hour(), t.Minute(), t.Second(), t.Nanosecond(), time.Local)
-		str := temp.Format(shortForm)
-		fmt.Println(str)
-	}
-
-	GetTimeFromStr()
-	GetTimeFromStrLoc()
 }
 
-func GetTimeFromStr() {
-	fmt.Println("\n GetTimeFromStr")
+//时间格式化显示
+func testTime2() {
+	t := time.Now()
+	t1 := t.String()
+	fmt.Println(t1) //2019-06-20 22:38:34.8048062 +0800 CST m=+0.005859201
+	timeStr := t1[:19]
+	fmt.Println(timeStr) //2019-06-20 22:38:34
+	fmt.Printf("%d-%d-%d %d:%d;%d:%d %v ", t.Year(), t.Month(), t.Day(), t.Hour(), t.Minute(), t.Second(), t.Nanosecond(), time.Local)
+
+	//指定显示格式
+	const shortForm = "2006-01-06 15:04:05"
+	str := t.Format(shortForm)
+	fmt.Println(str)
+
+	//"2019-06-06T15:58:11+08:00"
+	fmt.Println(time.Now().Format("2006-01-02T15:04:05+08:00"))
+}
+
+//设置时间
+func testTime3() {
+	t := time.Now()
+	temp := time.Date(t.Year(), t.Month(), t.Day(), t.Hour(), t.Minute(), t.Second(), t.Nanosecond(), time.Local)
+	fmt.Println(temp)
+}
+
+//从字符串解析出时间
+func testTime4() {
 	const format = "2006-01-02 15:04:05"
 	timeStr := "2018-01-09 20:24:20"
 	p, err := time.Parse(format, timeStr)
 	if err == nil {
 		fmt.Println(p)
 	}
-}
 
-//带时区匹配，匹配当前时区的时间
-func GetTimeFromStrLoc() {
-	fmt.Println("\n GetTimeFromStrLoc")
-
+	//从Asia/Shanghai时间转CEST时间
 	loc, _ := time.LoadLocation("Asia/Shanghai")
 	const longForm = "Jan 2, 2006 at 3:04pm (MST)"
 	t, _ := time.ParseInLocation(longForm, "Jul 9, 2012 at 5:02am (CEST)", loc)
 	fmt.Println(t)
-	// Note: without explicit zone, returns time in given location.
+
 	const shortForm = "2006-Jan-02"
 	t, _ = time.ParseInLocation(shortForm, "2012-Jul-09", loc)
 	fmt.Println(t)
+}
 
-	//"2019-06-06T15:58:11+08:00"
-	fmt.Println(time.Now().Format("2006-01-02T15:04:05+08:00"))
+//时间加减运算
+func testTime5() {
+	t := time.Now()
+	until := t.Add(time.Second * time.Duration(5)) //加10秒
+	fmt.Println(t)
+	fmt.Println(until)
+	for time.Now().Before(until) {
+		fmt.Println("每 2 打印一次")
+		time.Sleep(time.Second * time.Duration(2))
+	}
 }
